621534af8ff5e281ed5801b796e5d2f38d68954f
thidaratwongsing11 <thidarat.wong.66@ubu.ac.th>
2024-11-05 16:01:04 +0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() }
+   // Intialize the LED pin as an output
+   pinMode(LED_BUILTIN, OUTPUT);
+ 
+ void loop() {
+  // put your setup code here, to run once:
+
+}
+  // Turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // wait for a second (1000 millisecond)
+
+  // Turn the LED off by making the voltege LOW
